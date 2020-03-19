b58d50cdc44b3498bd1dc1b7b9068f1323e983f8
Elvis Chidera <elvischidera@192.168.1.5>
2020-03-19 00:50:53 -0400

    RewardsManager skeletons start

--- app/src/main/java/com/coronago/rewards/RewardsManager.kt
+++ app/src/main/java/com/coronago/rewards/RewardsManager.kt
@@ -1,10 +1,15 @@
 package com.coronago.rewards
 
 import android.content.Context
+import android.content.SharedPreferences
+
+private const val KEY_POINTS = "KEY_POINTS"
+private const val TOP_UP_POINTS_AMOUNT = 10
 
 // Here, another manager
 class RewardsManager(
-    private val appContext: Context
+    private val appContext: Context,
+    private val sp: SharedPreferences
 ) {
 
     fun onFirstChallengeCompleted() {
@@ -13,5 +18,24 @@ class RewardsManager(
 
     fun onFirstChallengeFailed() {
 
+    }
+
+    fun onSecondChallengeCompleted() {
+        topUpPoints()
+    }
+
+    fun onSecondChallengeFailed() {
+
+    }
+
+    private fun topUpPoints() {
+        val currentPoint = getPoints()
+        sp.edit()
+            .putInt(KEY_POINTS, currentPoint + TOP_UP_POINTS_AMOUNT)
+            .apply()
+    }
+
+    fun getPoints(): Int {
+        return sp.getInt(KEY_POINTS, 0)
     }
 }
